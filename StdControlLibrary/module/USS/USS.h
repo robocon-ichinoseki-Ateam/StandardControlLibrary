0845e761f884bab5f8cd0e857878b90c5db8f4ef
Kotakku <Kotakkucu@gmail.com>
2018-07-11 20:18:57 +0900

    1.7.4 feat: 超音波センサモジュールを追加

---
+++ StdControlLibrary/module/USS/USS.h
@@ -0,0 +1,39 @@
+#ifndef USS_h
+#define USS_h
+
+#include "mbed.h"
+class USS
+{
+public:
+    USS(PinName echoPin, PinName trigPin, double timeout = calcuTimeOfDistance(4.0));
+    
+    static double calcuTimeOfDistance(double distance);
+    static double calcuTimeOfDistance_ms(double distance);
+    static double calcuTimeOfDistance_us(double distance);
+    
+    void setAutoUpdate(bool isOn);
+    void start();
+    bool isData();
+    double getDistance();
+    
+    void fall (void (*fptr)(void));
+    void rise (void (*fptr)(void));
+
+protected:
+    void isr_rise();
+    void isr_fall();
+    void timeout();
+    
+protected:
+    double _timeout;
+    Timer _timer;
+    Timeout _timeoutTimer;
+    DigitalOut  _trigPin;
+    InterruptIn _echoPin;
+    
+    double _distance;
+    bool _isData;
+    bool _isAutoUpdate;
+};
+
+#endif //USS_h
